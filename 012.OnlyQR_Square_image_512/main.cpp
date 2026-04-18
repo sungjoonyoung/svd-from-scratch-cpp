5a04b274b0b5c391de16aa626fa22f774abb1908
SungJoonyoung <sungso376@gmail.com>
2026-04-19 05:08:50 +0900

    끝

---
+++ 012.OnlyQR_Square_image_512/main.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<fstream>
+#include<sungso376_LA.hpp>
+#include<sungso376_debbuging.hpp>
+#include<sungso376_eigen.hpp>
+#include<sungso376_image.hpp>
+using namespace std;
+vector<double> big_S;
+vector<vector<double>> big_U,big_V;
+
+int main(void){
+   //init
+   int N;N=512;
+   cout<<"max iter=";int iter;cin>>iter;cout<<"\n";
+   
+   vector<vector<double>>A;
+   ifstream fin;fin.open("512_Mandrill.bmp",ios::binary);
+   A=image_func(fin);
+   for(int i=0;i<N;i++)for(int j=0;j<N;j++){
+      if(A[i][j]==0)A[i][j]+=1e-5;
+   }
+   cout<<"이미지 추출 완료\n";
+
+   //
+   auto ATA=mat_mul(mat_transpose(A),A);
+   auto eigen_ATA=eigen_export(ATA,iter,1);
+   cout<<"아이젠 벨류 벡터 추출 완료\n";
+   for(int i=0;i<eigen_ATA.size();i++)big_S.push_back(eigen_ATA[i].value);
+   for(int i=0;i<eigen_ATA.size();i++)big_V.push_back(eigen_ATA[i].vector);
+   big_U=mat_transpose(mat_mul(A,mat_transpose(big_V)));
+   // for(int i=0;i<A.size();i++){ // 지금 bigU는 row로 누어 있는 상태
+   //    for(int j=0;j<A.size();j++){
+   //       big_U[i][j]/=big_U[i];
+   //    }
+   // }
+
+   //
+
+   // auto re=mat_zero(A.size());
+   // for(int i=0;i<A.size();i++){
+   //    re=mat_plus(re,vecvec_to_mat(big_U[i],big_V[i]));
+   // }
+   // cout<<"big UT==";
+   // print_2D(big_U);
+   // cout<<"big S==";
+   // print_1D(big_S);
+   // cout<<"big VT==";
+   // print_2D(big_V);
+   // cout<<"USV==";
+   // print_2D(re);
+   while(1){
+      // cout<<"점? 누적? (1(점),2(누적),0(꺼짐)) : ";
+      int in;
+      in=2;
+      // cin>>in;
+      if(in==1){
+         cout<<"아이젠 몇을 보여드릴까요? : ";
+         int op;cin>>op;
+         op=min(max(0,op),N-1);
+         auto re=mat_zero(A.size());
+         re=mat_plus(re,vecvec_to_mat(big_U[op],big_V[op]));
+         // for(int i=0;i<N;i++)for(int j=0;j<N;j++)re[i][j]*=128.0;
+         ofstream fout;fout.open("tmp.bmp",ios::binary);
+         saveBMP(fout,re);
+         cout<<"\n완료! tmp.bmp를 확인해보세요\n";
+      }
+      else if(in==2){
+         cout<<"아이젠 몇까지 보여드릴까요? : ";
+         int op;cin>>op;
+         op=min(max(0,op),N-1);
+         auto re=mat_zero(A.size());
+         for(int i=0;i<op;i++){
+            re=mat_plus(re,vecvec_to_mat(big_U[i],big_V[i]));
+         }
+         ofstream fout;fout.open("tmp.bmp",ios::binary);
+         saveBMP(fout,re);
+         cout<<"\n완료! tmp.bmp를 확인해보세요\n";
+      }
+      else break;
+   }
+}
+/*
+1 2 3
+5 4 1
+5 3 1
+*/
+
+/*
+1 5 2 6
+7 4 2 1
+5 8 5 3
+6 2 1 8
+*/
